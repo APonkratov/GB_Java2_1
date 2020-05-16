703817102f9dd1d9b883fd2d68250a34fd0bafdf
Aleksandr Ponkratov <aleks.ponkratov@gmail.com>
2020-05-17 02:40:56 +0300

    1-st homework

---
+++ src/com/geekbrains/java2/lesson1/homework/Motion.java
@@ -0,0 +1,5 @@
+package com.geekbrains.java2.lesson1.homework;
+
+public enum Motion {
+    JUMP, RUN
+}
---
+++ src/com/geekbrains/java2/lesson1/homework/Wall.java
@@ -0,0 +1,7 @@
+package com.geekbrains.java2.lesson1.homework;
+
+public class Wall extends Obstacle {
+    public Wall(double height) {
+        super(Motion.JUMP, height);
+    }
+}
